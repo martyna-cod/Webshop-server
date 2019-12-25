80f46ae701a7c1f0771bdfc51716d4155b166244
martyna-cod <lamek.martyna@gmail.com>
2019-12-25 09:37:17 +0100

    add .gitignore, install dependencies, express app

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => res.send("Good morning"))
+app.listen(port, () => console.log(`Listening on port ${port}`))
