985d993e21ddc82fe41d1828aacf60278da6d675
martyna-cod <lamek.martyna@gmail.com>
2020-02-16 11:22:21 +0100

    get product router, include Category

--- Product/router.js
+++ Product/router.js
@@ -7,7 +7,7 @@ const { toData } = require("../auth/jwt");
 const router = new Router();
 
 router.get("/product", (req, res, next) => {
-  Product.findAll(({include: [User]}))
+  Product.findAll(({include: [Category]}))
     .then(product => {
       res.json(product);
     })
